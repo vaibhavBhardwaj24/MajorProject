e7374d12860b3c48952a7515d7c4f0158a70db34
Pooja Yadav <py028203@gmail.com>
2025-05-17 22:51:18 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+.then(() => {
+    console.log("connected to DB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+   await Listing.deleteMany({});
+   initdata.data = initdata.data.map((obj) => ({ ...obj, owner: "6821911e299014786740b4a6"}));
+   await Listing.insertMany(initdata.data);
+   console.log("data was initialised");
+};
+
+initDB();
