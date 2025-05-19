486eafbb6aa2200e79b6a9f815edb3e024539a8e
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2025-05-20 00:43:57 +0530

    env fix

--- init/index.js
+++ init/index.js
@@ -1,23 +1,27 @@
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 main()
-.then(() => {
+  .then(() => {
     console.log("connected to DB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-   await Listing.deleteMany({});
-   initdata.data = initdata.data.map((obj) => ({ ...obj, owner: "6821911e299014786740b4a6"}));
-   await Listing.insertMany(initdata.data);
-   console.log("data was initialised");
+  await Listing.deleteMany({});
+  initdata.data = initdata.data.map((obj) => ({
+    ...obj,
+    owner: "6821911e299014786740b4a6",
+  }));
+  await Listing.insertMany(initdata.data);
+  console.log("data was initialised");
 };
 
 initDB();
